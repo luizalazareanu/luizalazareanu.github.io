f149070b4198b66df11602cc6f3298793a8212a4
Luiza <luiza.lazareanu@simplica.com>
2020-08-10 19:20:26 +0300

    server requests

---
+++ js/serverRequests.js
@@ -0,0 +1,53 @@
+/************************************* CART REQUESTS ******************/
+///// GET cart books from server
+function fetchBooksForCart() {
+    return fetch("http://localhost:3000/booksForCart", {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+    }).then(response => response.json());
+}
+
+
+/// calculate the number of items that are currently into cart
+function getNoOfCartItems(cartBooks) {
+    setTimeout(function () {
+        var bagTotal = document.getElementById("bagTotal");
+        //console.log(bagTotal);
+        bagTotal.innerHTML = cartBooks.length;
+    }, 100)
+}
+
+/// POST cart book on server
+function postBookInCart(bodyValue) {
+    return fetch("http://localhost:3000/booksForCart", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        }, body: bodyValue
+    })
+}
+
+/************************************* WISHLIST REQUESTS ******************/
+function fetchBooksForWishlist() {
+    return fetch("http://localhost:3000/booksForWishlist", {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+    }).then(response => response.json());
+}
+
+
+/// POST wishlist book on server
+function postBookInWishlist(bodyValue) {
+    return fetch("http://localhost:3000/booksForWishlist", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        }, body: bodyValue
+    })
+}
