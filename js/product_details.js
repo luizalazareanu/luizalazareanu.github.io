7413886bbaf0bf5fa6ba054e077c78f91ec2f2bd
Luiza <luiza.lazareanu@simplica.com>
2020-08-28 14:36:42 +0300

    buttons animation

--- js/product_details.js
+++ js/product_details.js
@@ -12,53 +12,6 @@ button.addEventListener("click",function(){
     div.classList.toggle('hidden-paragraph');
 });
 
-//fly to cart functionality on click
-function getCartPosition() {
-    setTimeout(function () {
-        var cartIcon = document.getElementById("cart-icon");
-        var yPosition = window.scrollY + cartIcon.getBoundingClientRect().top;
-        var xPosition = window.scrollX + cartIcon.getBoundingClientRect().left;
-        //console.log(xPosition);
-        //console.log(yPosition);
-        document.getElementById("add-to-cart").animate([
-            // keyframes
-            {transform: `translateX(${xPosition}px)`},
-            {transform: `translateY(${yPosition}px)`}
-        ], {
-            // timing options
-            //duration: 1000,
-            //iterations: Infinity
-        });
-    }, 1000)
-}
-
-// var yPosition = window.scrollY + cartIcon.getBoundingClientRect().top;
-// var xPosition = window.scrollX + cartIcon.getBoundingClientRect().left;
-// console.log(yPosition);
-// console.log(xPosition);
-getCartPosition();
-
-document.getElementById('add-to-cart').addEventListener("click", function (e) {
-    e.preventDefault;
-    // -> removing the class
-    document.getElementById('add-to-cart').classList.remove("animate-add-to-cart");
-    // -> triggering reflow
-    void document.getElementById('add-to-cart').offsetWidth;
-    // ->re-adding the class
-    document.getElementById('add-to-cart').classList.add("animate-add-to-cart");
-}, false);
-// add to wishlist functionality on click
-document.getElementById('add-to-wishlist').addEventListener("click", function (e) {
-    e.preventDefault;
-    // -> removing the class
-    document.getElementById('add-to-wishlist').classList.remove("animate-add-to-wishlist");
-    // -> triggering reflow
-    void document.getElementById('add-to-wishlist').offsetWidth;
-    // ->re-adding the class
-    document.getElementById('add-to-wishlist').classList.add("animate-add-to-wishlist");
-}, false);
-
-
 ///set book cover, title, author and price from local storage (comes from products page)
 var bookIdContainer = document.querySelector(".cover-container");
 var frontCover = document.getElementById("front-cover");
